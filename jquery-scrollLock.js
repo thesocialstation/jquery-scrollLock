35d50f675fd313f6dc3e9e931f69dacb1023f8c3
Ron Heft <ron@thesocialstation.com>
2015-11-18 11:58:27 -0500

    Support horizontal scroll

--- jquery-scrollLock.js
+++ jquery-scrollLock.js
@@ -34,23 +34,35 @@
           //allow zooming
           if (!event.ctrlKey) {
             var $this = $(this);
-            if (force === true || hasVerticalScroll($this)) {
+            if (force === true || hasVerticalScroll($this) || hasHorizontalScroll($this)) {
               //Support for nested scrollable blocks (see https://github.com/MohammadYounes/jquery-scrollLock/issues/4)
               event.stopPropagation();
               var scrollTop = $this.scrollTop(),
                   scrollHeight = $this.prop('scrollHeight'),
                   clientHeight = $this.prop('clientHeight'),
-                  delta = event.originalEvent.wheelDelta || (-1 * event.originalEvent.detail) || (-1 * event.originalEvent.deltaY),
-                  deltaY = 0
-              ;
-              if (event.type === "wheel") {
-                var ratio = $this.height() / $(window).height();
-                deltaY = event.originalEvent.deltaY * ratio;
+                  deltaForY = event.originalEvent.wheelDelta || (-1 * event.originalEvent.detail) || (-1 * event.originalEvent.deltaY),
+                  deltaY = 0,
+                  scrollLeft = $this.scrollLeft(),
+                  scrollWidth = $this.prop('scrollWidth'),
+                  clientWidth = $this.prop('clientWidth'),
+                  deltaForX = event.originalEvent.wheelDelta || (-1 * event.originalEvent.detail) || (-1 * event.originalEvent.deltaX),
+                  deltaX = 0
+                  ;
+              if (event.type === eventName) {
+                var ratioForY = $this.height() / $(window).height();
+                deltaY = event.originalEvent.deltaY * ratioForY;
+                var ratioForX = $this.width() / $(window).width();
+                deltaX = event.originalEvent.deltaX * ratioForX;
               }
-              if (delta > 0 && scrollTop + deltaY <= 0 || delta < 0 && scrollTop + deltaY >= scrollHeight - clientHeight) {
+              if (deltaForY > 0 && scrollTop + deltaY <= 0 || deltaForY < 0 && scrollTop + deltaY >= scrollHeight - clientHeight ||
+                  deltaForX > 0 && scrollLeft + deltaX <= 0 || deltaForX < 0 && scrollLeft + deltaX >= scrollWidth - clientWidth) {
                 event.preventDefault();
-                if (deltaY)
-                  $this.scrollTop(scrollTop + deltaY);
+                //if (deltaY) {
+                $this.scrollTop(scrollTop + deltaY);
+                //}
+                //if (deltaX) {
+                $this.scrollLeft(scrollLeft + deltaX);
+                //}
               }
             }
           }
@@ -62,9 +74,18 @@
         offsetWidth = $element.prop('offsetWidth'),
         borderRightWidth = parseInt($element.css('border-right-width'), 10),
         borderLeftWidth = parseInt($element.css('border-left-width'), 10)
-    ;
+        ;
 
     return clientWidth + borderLeftWidth + borderRightWidth < offsetWidth;
+  }
+  function hasHorizontalScroll($element) {
+    var clientHeight = $element.prop('clientHeight'),
+        offsetHeight = $element.prop('offsetHeight'),
+        borderTopWidth = parseInt($element.css('border-top-width'), 10),
+        borderBottomWidth = parseInt($element.css('border-bottom-width'), 10)
+        ;
+
+    return clientHeight + borderTopWidth + borderBottomWidth < offsetHeight;
   }
   // no conflict
   $.fn.scrollLock.noConflict = function () {
